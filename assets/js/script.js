76dd91b11584ee1747568a45536653a69384b950
jschnapper <jschnap717@gmail.com>
2017-10-09 10:00:17 -0700

    YEAH

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	d3.select("p").text("tomato tomato");
+	// d3.select("p").text("tomato tomato");
 
 	// https://scrimba.com/casts/cast-1953 for me to learn how it works
 	var numbers = [30, 86, 168, 281, 303, 365];
@@ -26,28 +26,26 @@ $(document).ready(function() {
 			// .append(".bars")
 			// .attr("width", function(d) {return d.TotalC2010/100000})
 			// .attr("y", function(d, i) {return i*10});
-   //  });  
+   //  });
 
+// FROM CHART.JS
 
     var ctx = $(".chart1");
     var chart = new Chart(ctx, {
-        // The type of chart we want to create
         type: 'line',
-
-        // The data for our dataset
         data: {
             labels: ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM", "1:00 PM", "2:00 PM", "3:00 PM"],
             datasets: [{
-                label: "My First dataset",
+                label: "Ida Sproul Energy Consumption",
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
-                data:[0, 10, 5, 2, 20, 30, 45],
-            }]
+                data:[30, 42, 38, 35, 41, 27, 25],
+                fill: false,
+        }],
+            options: {}
         },
 
-        // Configuration options go here
-        options: {}
-        });
+    });
 
 
 
