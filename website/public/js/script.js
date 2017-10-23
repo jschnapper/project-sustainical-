cf330bb2fe805f5459d9a82114346e8a8b1e9548
Daniel Farias <daniel4farias@gmail.com>
2017-10-23 16:19:53 -0700

    CSV Reader (with bugs)
    Version 0.0

--- website/public/js/script.js
+++ website/public/js/script.js
@@ -1,55 +1,55 @@
-$(document).ready(function() {
-  console.log('this was read')
+function getDataPointsFromCSV(csv) {
+    var dataPoints = csvLines = points = [];
+    csvLines = csv.split(/[\r?\n|\r|\n]+/);
+        
+    for (var i = 1; i < csvLines.length; i++)
+        if (csvLines[i].length > 0) {
+            points = csvLines[i].split(",");
+            dataPoints.push({ 
+                x: parseFloat(points[3]), 
+                y: parseFloat(points[4])    
+      });
+  }
+    return dataPoints;
+}
 
+var dataPoints = [];
+
+$(document).ready(function() {
 	// d3.select("p").text("tomato tomato");
 
 	// https://scrimba.com/casts/cast-1953 for me to learn how it works
-	var numbers = [30, 86, 168, 281, 303, 365];
 
-	d3.select(".chart")
-  		.selectAll("div")
-  		.data(numbers)
-    		.enter()
-    		.append("div")
-    		.classed("more", true)
-    		.style("width", function(d) { return d + "px"; })
-    		.text(function(d) { return d; });
+	// d3.select(".chart")
+ //  		.selectAll("div")
+ //  		.data(dataPoints)
+ //    		.enter()
+ //    		.append("div")
+ //    		.classed("more", true)
+ //    		.style("width", function(d) { return d + "px"; })
+ //    		.text(function(d) { return d; });
 
-
-    // HOME
-
-   //  d3.csv("energy.csv", function(data) {
-   //  	console.log("good");
-   //      var chart = d3.select("#energy-chart");
-   //      chart.selectAll(".bars")
-   //  		.data(data)
-   //  		.enter()
-			// .append(".bars")
-			// .attr("width", function(d) {return d.TotalC2010/100000})
-			// .attr("y", function(d, i) {return i*10});
-   //  });
-
-// FROM CHART.JS
-
-    var ctx = $(".chart1");
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM", "1:00 PM", "2:00 PM", "3:00 PM"],
-            datasets: [{
-                label: "Ida Sproul Energy Consumption",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data:[30, 42, 38, 35, 41, 27, 25],
-                fill: false,
-        }],
-            options: {}
+  //  var ctx = $(".chart1");
+    var chart = new Chart("chartContainer", {
+    // var chart = new Chart(ctx, {
+        title: {
+      text: "Chart from CSV",
         },
+        data: [{
+      type: "line",
+      dataPoints: getDataPointsFromCSV('energy.csv')
+       }]
+       // data: {
+       //     labels: ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM", "1:00 PM", "2:00 PM", "3:00 PM"],
+       //     datasets: [{
+       //         label: "Ida Sproul Energy Consumption",
+       //         backgroundColor: 'rgb(255, 99, 132)',
+       //         borderColor: 'rgb(255, 99, 132)',
+       //         data:[30, 42, 38, 35, 41, 27, 25],
+       //         fill: false,
+       // }],
 
     });
-
-
-
+    chart.render();
 });
-
-
+ 
