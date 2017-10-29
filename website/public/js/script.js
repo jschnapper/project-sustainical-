710e480d39d0225583596404e5c5e6e386ec95b8
jschnapper <jschnap717@gmail.com>
2017-10-28 21:20:29 -0700

    Get Ready

--- website/public/js/script.js
+++ website/public/js/script.js
@@ -1,55 +1,88 @@
+// function getDataPointsFromCSV(csv) {
+//     var dataPoints = csvLines = points = [];
+//     csvLines = csv.split(/[\r?\n|\r|\n]+/);
+        
+//     for (var i = 1; i < csvLines.length; i++)
+//         if (csvLines[i].length > 0) {
+//             points = csvLines[i].split(",");
+//             dataPoints.push({ 
+//                 x: parseFloat(points[3]), 
+//                 y: parseFloat(points[4])    
+//       });
+//   }
+//     return dataPoints;
+// }
+
+// var dataPoints = [];
+
+// $(document).ready(function() {
+// 	// d3.select("p").text("tomato tomato");
+
+// 	// https://scrimba.com/casts/cast-1953 for me to learn how it works
+
+// 	// d3.select(".chart")
+//  //  		.selectAll("div")
+//  //  		.data(dataPoints)
+//  //    		.enter()
+//  //    		.append("div")
+//  //    		.classed("more", true)
+//  //    		.style("width", function(d) { return d + "px"; })
+//  //    		.text(function(d) { return d; });
+
+//   //  var ctx = $(".chart1");
+//     var chart = new CanvasJS.Chart("chartContainer", {
+//     // var chart = new Chart(ctx, {
+//         title: {
+//       text: "Chart from CSV",
+//         },
+//         data: [{
+//       type: "line",
+//       dataPoints: getDataPointsFromCSV('energy.csv')
+//        }]
+//        // data: {
+//        //     labels: ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM", "1:00 PM", "2:00 PM", "3:00 PM"],
+//        //     datasets: [{
+//        //         label: "Ida Sproul Energy Consumption",
+//        //         backgroundColor: 'rgb(255, 99, 132)',
+//        //         borderColor: 'rgb(255, 99, 132)',
+//        //         data:[30, 42, 38, 35, 41, 27, 25],
+//        //         fill: false,
+//        // }],
+
+//     });
+//     chart.render();
+// });
+
+
+var dataPoints = [];
+ 
 function getDataPointsFromCSV(csv) {
     var dataPoints = csvLines = points = [];
     csvLines = csv.split(/[\r?\n|\r|\n]+/);
         
-    for (var i = 1; i < csvLines.length; i++)
+    for (var i = 0; i < csvLines.length; i++)
         if (csvLines[i].length > 0) {
             points = csvLines[i].split(",");
             dataPoints.push({ 
-                x: parseFloat(points[3]), 
-                y: parseFloat(points[4])    
+                x: parseFloat(points[0]), 
+                y: parseFloat(points[1])    
       });
   }
     return dataPoints;
 }
-
-var dataPoints = [];
-
-$(document).ready(function() {
-	// d3.select("p").text("tomato tomato");
-
-	// https://scrimba.com/casts/cast-1953 for me to learn how it works
-
-	// d3.select(".chart")
- //  		.selectAll("div")
- //  		.data(dataPoints)
- //    		.enter()
- //    		.append("div")
- //    		.classed("more", true)
- //    		.style("width", function(d) { return d + "px"; })
- //    		.text(function(d) { return d; });
-
-  //  var ctx = $(".chart1");
-    var chart = new Chart("chartContainer", {
-    // var chart = new Chart(ctx, {
+   
+$.get("dataPoints.csv", function(data) {
+    var chart = new CanvasJS.Chart("chartContainer", {
         title: {
       text: "Chart from CSV",
         },
         data: [{
       type: "line",
-      dataPoints: getDataPointsFromCSV('energy.csv')
-       }]
-       // data: {
-       //     labels: ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM", "1:00 PM", "2:00 PM", "3:00 PM"],
-       //     datasets: [{
-       //         label: "Ida Sproul Energy Consumption",
-       //         backgroundColor: 'rgb(255, 99, 132)',
-       //         borderColor: 'rgb(255, 99, 132)',
-       //         data:[30, 42, 38, 35, 41, 27, 25],
-       //         fill: false,
-       // }],
-
+      dataPoints: getDataPointsFromCSV(data)
+  }]
     });
+    
     chart.render();
+ 
 });
  
