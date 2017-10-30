097c1824f793d0b7e67bea5dc8c0e4cce99d66c7
jschnapper <jschnap717@gmail.com>
2017-10-29 21:35:24 -0700

    dashboard and css

--- website/app.js
+++ website/app.js
@@ -17,6 +17,97 @@ var db = mongoose.connection;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// // FOR MONGODB ////////
+// var MongoClient = require('mongodb').MongoClient
+//   , assert = require('assert');
+
+// // Connection URL
+// var url = 'mongodb://localhost:27017/myproject';
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+
+//   insertDocuments(db, function() {
+//     indexCollection(db, function() {
+//       db.close();
+//     });
+//   });
+// });
+
+
+
+// // 2nd phase of MONGODB
+// var insertDocuments = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Insert some documents
+//   collection.insertMany([
+//     {a : 1}, {a : 2}, {a : 3}
+//   ], function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(3, result.result.n);
+//     assert.equal(3, result.ops.length);
+//     console.log("Inserted 3 documents into the collection");
+//     callback(result);
+//   });
+// }
+
+// // 3rd phase of MONGODB
+// var findDocuments = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Find some documents
+//   collection.find({'a': 3}).toArray(function(err, docs) {
+//     assert.equal(err, null);
+//     console.log("Found the following records");
+//     console.log(docs);
+//     callback(docs);
+//   });      
+// }
+
+// // 4th phase of MONGODB
+// var updateDocument = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Update document where a is 2, set b equal to 1
+//   collection.updateOne({ a : 2 }
+//     , { $set: { b : 1 } }, function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(1, result.result.n);
+//     console.log("Updated the document with the field a equal to 2");
+//     callback(result);
+//   });  
+// }
+
+// // 5th phase of MONGODB
+// var removeDocument = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Delete document where a is 3
+//   collection.deleteOne({ a : 3 }, function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(1, result.result.n);
+//     console.log("Removed the document with the field a equal to 3");
+//     callback(result);
+//   });    
+// }
+
+// // 6th phase of MONGODB
+// var indexCollection = function(db, callback) {
+//   db.collection('documents').createIndex(
+//     { "a": 1 },
+//       null,
+//       function(err, results) {
+//         console.log(results);
+//         callback();
+//     }
+//   );
+// };
+
+// // END MONGODB ///////
+
+
 // Init App
 var app = express();
 
