b2887f5a7bcb5f848639f4681291a5b16822110b
Daniel Farias <daniel4farias@gmail.com>
2017-11-26 15:38:30 -0700

    Local
    Stuff

--- website/routes/users.js
+++ website/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+//var username = // ("#username").html()
 var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
