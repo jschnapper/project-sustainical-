f9cb11fb30e12b3cd9ac556e72a13e5602375965
jschnapper <jschnap717@gmail.com>
2017-11-27 02:33:40 -0800

    other

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set Static Folder
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Express Session
 app.use(session({
